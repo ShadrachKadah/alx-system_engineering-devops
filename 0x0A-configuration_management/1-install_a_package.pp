3a0da31ea0d4c274fa01887724e8566e9b867aec
ShadrachKadah <shadrachaudukadah1@gmail.com>
2024-03-22 07:28:03 -0700

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Ensure python3-pip is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Define a package resource for Flask version 2.1.0
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
 }
